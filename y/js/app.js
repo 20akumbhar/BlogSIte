608f47e94f24927d19a73d24548f9f888a77d21a
20akumbhar <20akumbhar@gmail.com>
2019-12-23 11:28:24 +0530

    initail commit.......

---
+++ y/js/app.js
@@ -0,0 +1,40 @@
+$(".menu-toggle").on("click",function(){
+//alert("clicked");
+$("header ul").toggleClass("showing");
+});
+$('.slider-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
